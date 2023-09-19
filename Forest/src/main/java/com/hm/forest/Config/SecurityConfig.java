1a1003f865a57f79808a18b61af6f83f85c68505
DESKTOP-DP3RJ5I\hi <dydgus9899@naver.com>
2023-09-19 16:51:24 +0900

    로그인 시큐리티 적용 완료 및 암호화 완료

--- Forest/src/main/java/com/hm/forest/Config/SecurityConfig.java
+++ Forest/src/main/java/com/hm/forest/Config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.hm.forest.Handler;
+package com.hm.forest.Config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -7,31 +7,54 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.hm.forest.Handler.CustomAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // ... 다른 설정
+    // 인메모리 사용자를 위한 사용자 서비스 빈 설정
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+        		.password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+        		.password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // 사용자 인증 정보 설정
+
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                // 권한 설정
                 .antMatchers("/home").authenticated()
                 .anyRequest().permitAll()
                 .and()
             .formLogin()
                 .loginPage("/login")
-                .successHandler(authenticationSuccessHandler()) // 로그인 성공 시 처리
-                .failureUrl("/login?error=true") // 로그인 실패 시 URL
+                .successHandler(authenticationSuccessHandler())
+                .failureUrl("/login?error=true")
                 .permitAll()
                 .and()
             .logout()
@@ -41,5 +64,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationSuccessHandler authenticationSuccessHandler() {
         return new CustomAuthenticationSuccessHandler();
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- Forest/src/main/java/com/hm/forest/Handler/CustomAuthenticationSuccessHandler.java
+++ Forest/src/main/java/com/hm/forest/Handler/CustomAuthenticationSuccessHandler.java
@@ -16,6 +16,6 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
             Authentication authentication) throws IOException, ServletException {
         // 로그인 성공 시 처리할 내용을 여기에 구현합니다.
         // 예를 들어, 로그인한 사용자를 홈 페이지로 리다이렉트하거나 특정 동작을 수행할 수 있습니다.
-        response.sendRedirect("/home");
+        response.sendRedirect("/");
     }
 }
