5a841341bd394146b695ebb44381eb7d1c70ea11
PC\82107 <liest1029@naver.com>
2023-09-20 16:56:13 +0900

    no message

--- Forest/src/main/java/com/hm/forest/board/model/vo/Board.java
+++ Forest/src/main/java/com/hm/forest/board/model/vo/Board.java
@@ -3,18 +3,28 @@ package com.hm.forest.board.model.vo;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 @Data
+@Builder // SQL 사용시 파라미터에 값을 쉽게 넣어주는 @
+@ToString // 객체의 값 확인을 위한 @
+@Entity(name = "board")
 @NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class Board {
-	@NonNull
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int no;
 	
 	private int rowNum;
@@ -44,54 +54,6 @@ public class Board {
 	private Date createDate;
 	
 	private Date modifyDate;
-
-	public int getNo() {
-		return no;
-	}
-
-	public void setNo(int no) {
-		this.no = no;
-	}
-
-	public String getWriterId() {
-		return writerId;
-	}
-
-	public void setWriterId(String writerId) {
-		this.writerId = writerId;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public int getReadCount() {
-		return readCount;
-	}
-
-	public void setReadCount(int readCount) {
-		this.readCount = readCount;
-	}
-
-	public Date getCreateDate() {
-		return createDate;
-	}
-
-	public void setCreateDate(Date createDate) {
-		this.createDate = createDate;
-	}
 	
 }
 
