888592b91001a2493c8670c5e6c02910ae67ffda
PC\82107 <liest1029@naver.com>
2023-09-21 16:57:11 +0900

    no message

--- Forest/src/main/java/com/hm/forest/HomeController.java
+++ Forest/src/main/java/com/hm/forest/HomeController.java
@@ -1,25 +1,11 @@
 package com.hm.forest;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.hm.forest.product.model.service.ProductService;
-import com.hm.forest.product.model.vo.Product;
 
 @Controller
 public class HomeController {
-	
-	
 	// home으로 이동
 	@GetMapping("/")
 	public ModelAndView home (ModelAndView modlAndView) {
@@ -27,6 +13,51 @@ public class HomeController {
 		modlAndView.setViewName("page/home");
     
         return modlAndView;
+	}
+	
+	// 캠페인 페이지_aesop으로 이동
+	@GetMapping("/campaign/aesop")
+	public ModelAndView aseop (ModelAndView modlAndView) {
+		modlAndView.addObject("pageName", "aesop");
+		modlAndView.setViewName("page/campaign/aesop");
+    
+        return modlAndView;
+	}
+	
+	// 캠페인 페이지_biotherm으로 이동
+	@GetMapping("/campaign/biotherm")
+	public ModelAndView biotherm (ModelAndView modlAndView) {
+		modlAndView.addObject("pageName", "biotherm");
+		modlAndView.setViewName("page/campaign/biotherm");
+		
+		return modlAndView;
+	}
+	
+	// 캠페인 페이지_brita으로 이동
+	@GetMapping("/campaign/brita")
+	public ModelAndView brita (ModelAndView modlAndView) {
+		modlAndView.addObject("pageName", "brita");
+		modlAndView.setViewName("page/campaign/brita");
+		
+		return modlAndView;
+	}
+	
+	// 캠페인 페이지_melixir으로 이동
+	@GetMapping("/campaign/melixir")
+	public ModelAndView melixir (ModelAndView modlAndView) {
+		modlAndView.addObject("pageName", "melixir");
+		modlAndView.setViewName("page/campaign/melixir");
+		
+		return modlAndView;
+	}
+	
+	// 캠페인 페이지_boonbastick으로 이동
+	@GetMapping("/campaign/boonbastick")
+	public ModelAndView boonbastick (ModelAndView modlAndView) {
+		modlAndView.addObject("pageName", "boonbastick");
+		modlAndView.setViewName("page/campaign/boonbastick");
+		
+		return modlAndView;
 	}
 	
 	// about으로 이동
@@ -49,12 +80,72 @@ public class HomeController {
 		return modlAndView;
 	}
 	
-	// 제품보기로 이동
-	@GetMapping("/product")
+	// 제품보기_주방으로 이동
+	@GetMapping("/product/kitchen")
+	public ModelAndView kitchen (ModelAndView modlAndView) {
+		
+		modlAndView.addObject("pageName", "kitchen");
+		modlAndView.setViewName("page/product/kitchen");
+		
+		return modlAndView;
+		}
+		
+	// 제품보기_패션으로 이동
+	@GetMapping("/product/style")
+	public ModelAndView style (ModelAndView modlAndView) {
+		
+		modlAndView.addObject("pageName", "style");
+		modlAndView.setViewName("page/product/style");
+		
+		return modlAndView;
+	}
+	
+	// 제품보기_친환경제품로 이동
+	@GetMapping("/product/eco")
 	public ModelAndView product (ModelAndView modlAndView) {
 		
-		modlAndView.addObject("pageName", "product");
-		modlAndView.setViewName("page/product");
+		modlAndView.addObject("pageName", "eco");
+		modlAndView.setViewName("page/product/eco");
+		
+		return modlAndView;
+	}
+	
+	// 소통하기_공지사항으로 이동
+	@GetMapping("/board/notice")
+	public ModelAndView notice (ModelAndView modlAndView) {
+		
+		modlAndView.addObject("pageName", "notice");
+		modlAndView.setViewName("page/board/notice");
+		
+		return modlAndView;
+	}
+	
+	// 소통하기_자주묻는질문으로 이동
+	@GetMapping("/board/faq")
+	public ModelAndView faq (ModelAndView modlAndView) {
+		
+		modlAndView.addObject("pageName", "faq");
+		modlAndView.setViewName("page/board/faq");
+		
+		return modlAndView;
+	}
+	
+	// 소통하기_자유게시판으로 이동
+	@GetMapping("/board/community")
+	public ModelAndView community (ModelAndView modlAndView) {
+		
+		modlAndView.addObject("pageName", "community");
+		modlAndView.setViewName("page/board/community");
+		
+		return modlAndView;
+	}
+	
+	// 소통하기_실천인증으로 이동
+	@GetMapping("/board/act")
+	public ModelAndView act (ModelAndView modlAndView) {
+		
+		modlAndView.addObject("pageName", "act");
+		modlAndView.setViewName("page/board/act");
 		
 		return modlAndView;
 	}
@@ -78,9 +169,6 @@ public class HomeController {
 		
 		return modlAndView;
 	}
-	
-	
-	
 	
 	// 관리자페이지_클래스관리로 이동
 	@GetMapping("/admin/programMgmt")
