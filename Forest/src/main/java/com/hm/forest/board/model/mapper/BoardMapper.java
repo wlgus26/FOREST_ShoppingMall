16f8dce4b060b10b9d85755cc9cea9dfbd68f66e
PC\82107 <liest1029@naver.com>
2023-09-25 12:51:39 +0900

    게시판 _ 조회수, 검색기능 완료

--- Forest/src/main/java/com/hm/forest/board/model/mapper/BoardMapper.java
+++ Forest/src/main/java/com/hm/forest/board/model/mapper/BoardMapper.java
@@ -13,11 +13,17 @@ import com.hm.forest.common.util.PageInfo;
 @Mapper
 public interface BoardMapper {
 
-	// 게시글 목록 수 카운트
+	// 게시판 타입별 게시글 전체 개수
 	int selectBoardCountByType(@Param("type") String type);
 
-	// 게시글 전체 목록 조회
+	// 게시판 타입별 게시글 전체 목록 조회
 	List<Board> selectBoardListsByType(@Param("type") String type, RowBounds bounds);
+	
+	// [검색값 o] 게시판 타입별 게시글 전체 개수
+	int selectBoardCountBySearchValue(@Param("type") String type, @Param("searchType") String searchType,@Param("keyWord") String keyWord);
+	
+	// [검색값 o] 게시판 타입별 게시글 전체 목록 조회
+	List<Board> selectBoardListsBySearchValue(@Param("type") String type, RowBounds bounds, @Param("searchType") String searchType, @Param("keyWord") String keyWord);
 
 	// 특정 게시글 조회
 	Board selectBoardByNo(@Param("no") int no);
@@ -30,6 +36,12 @@ public interface BoardMapper {
 
 	// 게시글 삭제
 	int updateStatus(@Param("no") int no,@Param("status") String status);
+
+	// 게시글 조회수 업데이트
+	int updateReadCount(@Param("no") int no);
+
+
+
 
 
 }
--- Forest/src/main/java/com/hm/forest/board/service/BoardService.java
+++ Forest/src/main/java/com/hm/forest/board/service/BoardService.java
@@ -7,11 +7,17 @@ import com.hm.forest.common.util.PageInfo;
 
 public interface BoardService {
 
-	// 게시글 전체 목록 조회(게시판 타입별로)
+	// 게시판 타입별 게시글 전체 목록 조회
 	List<Board> getBoardLists(String type, PageInfo pageInfo);
 
-	// 게시글 개수(게시판 타입별로)
+	// 게시판 타입별 게시글 전체 개수
 	int selectBoardCountByType(String type);
+
+	// [검색값 o] 게시판 타입별 게시글 전체 목록 조회
+	List<Board> getBoardListsBySearchValue(String type, PageInfo pageInfo, String searchType, String keyWord);
+
+	// [검색값 o] 게시판 타입별 게시글 전체 개수
+	int selectBoardCountBySearchValue(String type, String searchType, String keyWord);
 
 	// 특정 게시글 조회
 	Board getBoardByNo(int no);
@@ -21,5 +27,10 @@ public interface BoardService {
 
 	// 게시글 삭제
 	int delete(int no);
+
+	// 게시글 조회수 업데이트
+	int updateReadCount(int no);
+
+
 
 }
