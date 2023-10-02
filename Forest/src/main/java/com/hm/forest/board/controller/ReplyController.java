8034618aebd52100780af2a210468d2b39fc8e78
PC\82107 <liest1029@naver.com>
2023-10-03 01:59:10 +0900

    게시판_ 댓글 수정, 삭제 진행 중

--- Forest/src/main/java/com/hm/forest/board/controller/ReplyController.java
+++ Forest/src/main/java/com/hm/forest/board/controller/ReplyController.java
@@ -10,10 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.hm.forest.board.model.service.BoardService;
+import com.hm.forest.board.model.vo.Board;
 import com.hm.forest.board.model.vo.Reply;
 
 import lombok.extern.slf4j.Slf4j;
@@ -25,8 +28,8 @@ public class ReplyController {
 	@Autowired
 	private BoardService boardService;
 
-	// 댓글 조회
 	// 비동기 통신 응답
+	// 댓글 조회
 	@GetMapping("/{boardNo}")
 	public ResponseEntity<Map<String, Object>> professor(@PathVariable("boardNo") int boardNo) {
 		Map<String, Object> map = new HashMap<>();
@@ -39,7 +42,6 @@ public class ReplyController {
 			
 	
 	// 댓글 등록
-	@ResponseBody
 	@PostMapping("/enroll")
 	public ResponseEntity<Map<String, Object>> enroll(@RequestBody Reply reply)  {
 		int result = 0;
@@ -57,4 +59,32 @@ public class ReplyController {
 		
 		return ResponseEntity.ok(map);
 	}
+	
+	// 댓글 수정
+		@PostMapping("/update")
+		public ResponseEntity<Map<String, Object>> update(@RequestBody Reply reply)  {
+			int result = 0;
+			
+			System.out.println(reply);
+			
+			Map<String, Object> map = new HashMap<>();
+			
+			result = boardService.save(reply);
+			
+			map.put("resultCode", result);
+			map.put("reply", reply);
+		
+			return ResponseEntity.ok(map);
+		}
+		
+		
+		// 댓글 수정 테스트
+		@GetMapping("/updateTest")
+		public ModelAndView updateTest (ModelAndView modelAndView) {
+			
+			modelAndView.addObject("pageName", "boardUpdate");
+			modelAndView.setViewName("page/board/update");
+			
+			return modelAndView;
+		}
 }
